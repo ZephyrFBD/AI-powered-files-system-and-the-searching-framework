1e8f3f11808a27703d4faabf1c9f842379ad4b05
Zephyr <Zephyr142024@gmail.com>
2024-06-24 08:49:27 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+from transformers import pipeline
+from tdt import read_txt_files
+
+#遍历txt：test文件夹下所有子文件夹
+directory = r'.\test'
+out = read_txt_files(directory)
+print(out)
+#out为字典,(directory,content)
+input = list(out.values())
+
